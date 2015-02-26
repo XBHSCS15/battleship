8becae246cca1c2a659eec09b42b4bdfe7dbe107
Trevor Lounsbury <Tlounsbury53@gmail.com>
2015-02-26 12:23:32 -0500

    Matching with master

--- src/xbhs/battleship/gui/GUI.java
+++ src/xbhs/battleship/gui/GUI.java
@@ -1,11 +1,16 @@
-//Placeholder Class
+package xbhs.battleship.gui;
 
-package src.xbhs.battleship.gui;
+import javax.swing.JFrame;
 
 // Testing the new branch creation. 
 // It makes a cool green line on the side too. 
 // IT shows where i have added ITS COOL.
 public class GUI 
 {
-	
+    
+    public GUI() 
+    {
+        
+    }
+    
 }
