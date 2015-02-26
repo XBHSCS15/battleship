17449ec53eb6fda2ca589be0ffce75e103a93c6e
Trevor Lounsbury <Tlounsbury53@gmail.com>
2015-02-26 12:21:14 -0500

    Fixed issue

--- src/xbhs/battleship/gui/GUI.java
+++ src/xbhs/battleship/gui/GUI.java
@@ -1,11 +1,15 @@
-//Placeholder Class
 
-package src.xbhs.battleship.gui;
+package xbhs.battleship.gui;
 
 // Testing the new branch creation. 
 // It makes a cool green line on the side too. 
 // IT shows where i have added ITS COOL.
 public class GUI 
 {
-	
+    
+    public GUI() 
+    {
+        
+    }
+    
 }
