44b34999634d839b73acb5db7f635003ee02ffc5
Mohak Saxena <mohaksaxena@gmail.com>
2015-02-26 21:06:28 -0500

    Added ShipPlacement and some updates to Game

--- src/xbhs/battleship/game/Game.java
+++ src/xbhs/battleship/game/Game.java
@@ -1,12 +1,17 @@
 package xbhs.battleship.game;
 
+import xbhs.battleship.player.Player;
+
 public class Game
 {
 	private Space[][] grid1, grid2;
 	private Ship[] ships;
+	private Player[] thePlayers;
 
 	public Game(int rowSize, int colSize)
 	{
+		thePlayers = new Player[2];
+		
 		grid1 = new Space[rowSize][colSize];
 		grid2 = new Space[rowSize][colSize];
 		for (int i = 0; i < grid1.length; i++)
@@ -16,11 +21,6 @@ public class Game
 				grid2[i][j] = new Space();
 			}
 		
-		shipsInit();
-	}
-	
-	private void shipsInit()
-	{
 		ships = new Ship[5];
 		ships[0] = new Ship(2);
 		ships[1] = new Ship(3);
@@ -28,15 +28,10 @@ public class Game
 		ships[3] = new Ship(4);
 		ships[4] = new Ship(5);
 	}
-
-	public void resetGame()
+	
+	private void placeShips(Space[][] grid)
 	{
-		for (int i = 0; i < grid1.length; i++)
-			for (int j = 0; j < grid1[0].length; j++)
-			{
-				grid1[i][j] = new Space();
-				grid2[i][j] = new Space();
-			}
+		
 	}
 	
 	public Ship[] getShips()
@@ -52,5 +47,10 @@ public class Game
 				return false;
 		}
 		return true;
+	}
+	
+	public void makeMove(Move[] move)
+	{
+		
 	}
 }
