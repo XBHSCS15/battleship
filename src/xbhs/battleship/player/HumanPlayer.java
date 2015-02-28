beec8726eed6bee05884beff9f20b03540eafd65
Faraaz Nadeem <faraaz.nadeem@gmail.com>
2015-02-27 20:48:35 -0500

    Added NetworkPlayer, HumanPlayer
    Added the framework for these two classes. They will eventually enable
    multiplayer interaction between humans over the network.

---
+++ src/xbhs/battleship/player/HumanPlayer.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xbhs.battleship.player;
+
+import xbhs.battleship.game.*;
+
+/**
+ *
+ * @author Faraaz
+ */
+public class HumanPlayer extends NetworkPlayer
+{
+    public Move[] getMove(Space[][] grid)
+    {
+        return null;
+    }
+    public ShipPlacement[] getPlacement(Space[][] grid, Ship[] ships)
+    {
+        return null;
+    }
+}
