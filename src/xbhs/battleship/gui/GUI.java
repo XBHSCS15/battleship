a7276e67dfc7cff16e8de659c32420f0180f44f1
FieryXJoe <josephmsherry@comcast.net>
2015-03-03 12:00:28 -0500

    Draws 10x10 Grid

--- src/xbhs/battleship/gui/GUI.java
+++ src/xbhs/battleship/gui/GUI.java
@@ -6,12 +6,13 @@ import java.awt.GraphicsEnvironment;
 import processing.core.PApplet;
 
 public class GUI extends PApplet {
-
+ 
+  GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+  int width = gd.getDisplayMode().getWidth()/2;
+  int height = gd.getDisplayMode().getHeight()/2;
+	
   public void setup() 
   {
-	GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-	int width = gd.getDisplayMode().getWidth()/2;
-	int height = gd.getDisplayMode().getHeight()/2;
 	size(width,height);
     background(0);
   }
@@ -19,9 +20,13 @@ public class GUI extends PApplet {
   public void draw() 
   {
     stroke(255);
-    if (mousePressed) 
+    int sideLength = getHeight();
+    int startX = (getWidth() - sideLength) / 2;
+    int delta = sideLength / 10;
+    for(int i = 0; i < 11; i++)
     {
-      line(mouseX,mouseY,pmouseX,pmouseY);
+    	line(startX, i * delta, startX + sideLength, i * delta);
+    	line(i * delta + startX, 0, i * delta + startX, sideLength);
     }
   }
   
