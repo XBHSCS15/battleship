82c9ae90dddcdb920ec20831753a19cbec4a0f57
FieryXJoe <josephmsherry@comcast.net>
2015-03-04 10:19:25 -0500

    Changed structure and removed button code
    Instead of the main GUI class containing the code to render everything
    and the logic for everything it now simply contains an ArrayList of
    GUIElements each of which will have its render code called each frame.
    
    The GUIElement functionality is fairly basic currently eventually each
    element will need to be hooked up to an object or variable in the game
    logic branch. Ex: GridElement would have access to the grid variable in
    the main game, Ship elements would have access to their associated ships
    and the ships would have access to their GUIElement.
    
    Any element which needs to be associated with an object and not a
    variable should be initialized in that object's constructor.
    
    Removed the code for buttons and will add them as a new GUIElement next.

--- src/xbhs/battleship/gui/GUI.java
+++ src/xbhs/battleship/gui/GUI.java
@@ -16,116 +16,55 @@ public class GUI extends PApplet {
   int initWidth = gd.getDisplayMode().getWidth()/2;
   int initHeight = gd.getDisplayMode().getHeight()/2;
   boolean mousePressedLastFrame = false;
-  ArrayList<String> buttonTitles = new ArrayList<String>();
-  /**
-   * Placeholder variable stores if a grid space
-   * should be colored to test click detection
-   */
-  boolean[][] grid = new boolean[10][10];
+  ArrayList<GUIElement> guiElements = new ArrayList<GUIElement>();
+  
   @Override
   public void setup() 
   {
-	for(int i = 0; i < 10; i++)
-		Arrays.fill(grid[i], false);
 	size(initWidth,initHeight);
     background(0);
-    /**
-    buttonTitles.add("A button");
-    buttonTitles.add("Another button");
-    buttonTitles.add("A third button");
-    */
+    initGUIElements();
+    for(GUIElement e: guiElements)
+    	e.init();
   }
 
   @Override
   public void draw() 
   {
-    stroke(255);
-    int sideLength = getHeight();
-    int startX = (getWidth() - sideLength) / 2;
-    int delta = sideLength / 10;
-	// Fixes issues with truncation
-    sideLength = delta * 10;
-	if(mousePressed != mousePressedLastFrame && !mousePressedLastFrame)
+	if(mousePressed)
 	{
-		int[] coords = getGridSquare(mouseX, mouseY);
-		if(coords != null)
-			grid[coords[0]][coords[1]] = !grid[coords[0]][coords[1]];
+		Object[] elementsAtPoint = getElementsAtCoords(mouseX, mouseY);
+		if(elementsAtPoint != null)
+			for(Object e: elementsAtPoint)
+				((GUIElement)e).onClicked(mouseX, mouseY);
+
 	}
 	mousePressedLastFrame = mousePressed;
-    for(int i = 0; i < 10; i++)
-    	for(int j = 0; j < 10; j++)
-    		if(grid[i][j])
-    			fillSquare(i, j, Color.RED);
-    		else
-    			fillSquare(i, j, getForeground());
-    for(int i = 0; i < 11; i++)
-    {
-    	line(startX, i * delta, startX + sideLength, i * delta);
-    	line(i * delta + startX, 0, i * delta + startX, sideLength);
-    }
-    //addButtons();
+	for(GUIElement e: guiElements)
+    	e.drawElement();
   }
   
-  /**
-   * Gets the grid space a given coordinate is inside of
-   */
-  public int[] getGridSquare(int x, int y)
+  private Object[] getElementsAtCoords(int x, int y)
   {
-	int sideLength = getHeight();
-	int startX = (getWidth() - sideLength) / 2;
-	int delta = sideLength / 10;
-	// Fixes issues with truncation
-	sideLength = delta * 10;
-	if(x < startX || x > startX + sideLength)
-		return null;
-	int[] coords = new int[2];
-	for(int i = 0; i < 11; i++)
-		if(x < startX + (i * delta))
-		{
-			coords[0] = i;
-			break;
-		}
-	for(int i = 0; i < 11; i++)
-		if(y < i * delta)
-		{
-			coords[1] = i;
-			break;
-		}
-	/**
-	 * To avoid indexOutOfBoundsExceptions on the grid array
-	 */
-	coords[0] += coords[0] == 0 ? 0 : -1;
-	coords[1] += coords[1] == 0 ? 0 : -1;
-	return coords;
+	  ArrayList<GUIElement> temp = new ArrayList<GUIElement>();
+	  for(GUIElement e: guiElements)
+	  {
+		  int[][] coords = e.getCoords();
+		  if(x >= coords[0][0] && x <= coords[2][0])
+			  if(y >= coords[0][1] && y <= coords[2][1])
+				  temp.add(e);
+	  }
+	  return temp.toArray();
+  }
+
+  public void addGUIElement(GUIElement e)
+  {
+	  guiElements.add(e);
   }
   
-  public void fillSquare(int x, int y, Color color)
+  private void initGUIElements()
   {
-	  int sideLength = getHeight();
-	  int startX = (getWidth() - sideLength) / 2;
-	  int delta = sideLength / 10;
-	  // Fixes issues with truncation
-	  sideLength = delta * 10;
-	  fill(color.getRGB());
-	  beginShape();
-	  	vertex((float)x * delta + startX, (float) y * delta);
-	  	vertex((float)x * delta + startX, (float) (y + 1) * delta);
-	  	vertex((float)(x + 1) * delta + startX, (float) (y + 1) * delta);
-	  	vertex((float)(x + 1) * delta + startX, (float) y * delta);
-	  endShape();
-  }
-  
-  public void addButtons()
-  {
-	  int minX = 0, maxX = (getWidth() - getHeight()) / 2;
-	  int deltaY = getHeight()/buttonTitles.size();
-	  for(int i = 0; i < buttonTitles.size(); i++)
-		  addButton(minX, maxX, i * deltaY, i + 1 * deltaY, buttonTitles.get(i));
-  }
-  
-  public void addButton(int minX, int maxX, int minY, int maxY, String text)
-  {
-	  //TODO: Fill this out
+	  guiElements.add(new GridElement((getWidth() - getHeight()) / 2, 0, (getWidth() - getHeight()) / 2 + getHeight(), getHeight(), this));
   }
   
   public static void main(String args[]) 
---
+++ src/xbhs/battleship/gui/GridElement.java
@@ -0,0 +1,106 @@
+package xbhs.battleship.gui;
+
+import java.awt.Color;
+import java.util.Arrays;
+
+public class GridElement extends GUIElement
+{
+	/**
+	 * Placeholder variable stores if a grid space
+	 * should be colored to test click detection
+	 */
+	boolean[][] grid = new boolean[10][10];
+	
+	public GridElement(int minX, int minY, int maxX, int maxY, GUI gui)
+	{
+		super(minX, minY, maxX, maxY, gui);
+	}
+
+	@Override
+	public void init() 
+	{
+		for(int i = 0; i < 10; i++)
+			Arrays.fill(grid[i], false);
+	}
+	
+	@Override
+	public void drawElement() 
+	{
+		int sideLength = getCoords()[2][0] - getCoords()[0][0];
+		int startX = getCoords()[0][0];
+		int delta = sideLength / 10;
+	    for(int i = 0; i < 10; i++)
+	    	for(int j = 0; j < 10; j++)
+	    		if(grid[i][j])
+	    			fillSquare(i, j, Color.RED);
+	    		else
+	    			fillSquare(i, j, getGUI().getForeground());
+		for(int i = 0; i < 11; i++)
+		{
+			if(i == 11)
+			{
+				getGUI().line(getCoords()[0][0], getCoords()[0][1], getCoords()[3][0], getCoords()[3][1]);
+				getGUI().line(getCoords()[2][0], getCoords()[2][1], getCoords()[3][0], getCoords()[3][1]);
+			}
+			getGUI().line(startX, i * delta, startX + sideLength, i * delta);
+			getGUI().line(i * delta + startX, 0, i * delta + startX, sideLength);
+		}
+	}
+
+	@Override
+	public void onClicked(int x, int y) 
+	{
+		if(getGUI().mousePressed != getGUI().mousePressedLastFrame && !getGUI().mousePressedLastFrame)
+		{
+			int[] coords = getGridSquare(getGUI().mouseX, getGUI().mouseY);
+			if(coords != null)
+				grid[coords[0]][coords[1]] = !grid[coords[0]][coords[1]];
+		}
+	}
+
+	public int[] getGridSquare(int x, int y)
+	{
+		int sideLength = getCoords()[2][0] - getCoords()[0][0];
+		int startX = getCoords()[0][0];
+		int delta = sideLength / 10;
+		// Fixes issues with truncation
+		sideLength = delta * 10;
+		if(x < startX || x > startX + sideLength)
+			return null;
+		int[] coords = new int[2];
+		for(int i = 0; i < 11; i++)
+			if(x < startX + (i * delta))
+			{
+				coords[0] = i;
+				break;
+			}
+		for(int i = 0; i < 11; i++)
+			if(y < i * delta)
+			{
+				coords[1] = i;
+				break;
+			}
+		/**
+		 * To avoid indexOutOfBoundsExceptions on the grid array
+		 */
+		coords[0] += coords[0] == 0 ? 0 : -1;
+		coords[1] += coords[1] == 0 ? 0 : -1;
+		return coords;
+	}
+
+	public void fillSquare(int x, int y, Color color)
+	{
+		int sideLength = getCoords()[2][0] - getCoords()[0][0];
+		int startX = getCoords()[0][0];
+		int delta = sideLength / 10;
+		// Fixes issues with truncation
+		sideLength = delta * 10;
+		getGUI().fill(color.getRGB());
+		getGUI().beginShape();
+		getGUI().vertex((float)x * delta + startX, (float) y * delta);
+		getGUI().vertex((float)x * delta + startX, (float) (y + 1) * delta);
+		getGUI().vertex((float)(x + 1) * delta + startX, (float) (y + 1) * delta);
+		getGUI().vertex((float)(x + 1) * delta + startX, (float) y * delta);
+		getGUI().endShape();
+	}
+}
